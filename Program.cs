6b1a16d0b25e69275500cda9ceebaa481cdcbe06
JPach001 <142937756+JPach001@users.noreply.github.com>
2023-09-07 17:24:41 -0600

    Se agrega el proyecto

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Sintáxis_2;
+
+namespace Sintaxis_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Token T = new Token();
+
+                T.setContenido("3.5e-8");
+                T.setClasificacion(Token.Tipos.Numero);
+
+                Console.WriteLine(T.getContenido());
+                Console.WriteLine(T.getClasificacion());
+
+                /*using (Lenguaje L = new Lenguaje("suma.cpp"))
+                {
+                    //L.Programa();
+                    
+                    while (!L.FinArchivo())
+                    {
+                        L.nextToken();
+                    }
+                    
+                }*/
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: "+e.Message);
+            }
+        }
+    }
+}
