644c7831bc92a58582bc549cae8218d89fde3546
JPach001 <julianpacheco01@hotmail.com>
2023-09-10 15:42:22 -0600

    "Se agregan el resto de archivos faltantes"

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Sintáxis_2;
 
 namespace Sintaxis_2
 {
@@ -9,28 +8,20 @@ namespace Sintaxis_2
         {
             try
             {
-                Token T = new Token();
-
-                T.setContenido("3.5e-8");
-                T.setClasificacion(Token.Tipos.Numero);
-
-                Console.WriteLine(T.getContenido());
-                Console.WriteLine(T.getClasificacion());
-
-                /*using (Lenguaje L = new Lenguaje("suma.cpp"))
+                using (Lenguaje L = new Lenguaje("suma.cpp"))
                 {
-                    //L.Programa();
                     
-                    while (!L.FinArchivo())
+                    L.Programa();
+
+                    /*while (!L.FinArchivo())
                     {
                         L.nextToken();
-                    }
-                    
-                }*/
-            } 
+                    }*/
+                }
+            }
             catch (Exception e)
             {
-                Console.WriteLine("Error: "+e.Message);
+                Console.WriteLine("Error: " + e.Message);
             }
         }
     }
