12265a0714a65affc73bd1e254b453a41db35831
JPach001 <julianpacheco01@hotmail.com>
2023-11-08 23:58:34 -0600

    "Implementacion requerimientos 1-3"

--- Lenguaje.cs
+++ Lenguaje.cs
@@ -6,7 +6,6 @@ using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 /*
-
     Requerimiento 1: Programar scanf 
     Requerimiento 2: Programar printf
     Requerimiento 3: Programar ++,--,+=,-=,*=,/=,%=
